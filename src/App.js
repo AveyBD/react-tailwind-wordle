e18ce72c538a1c578bfa148dbe30b85669ccb2ef
Debashish D. Dev <hello@debashish.dev>
2022-06-12 17:54:10 +0600

    Data From Local Server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [solution, setSolution] = useState(null);
+  const [ans, setAns] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/solutions")
       .then((res) => res.json())
       .then((data) => {
         const sol = Math.floor(Math.random() * data.length);
-        setSolution(data[sol]);
+        setAns(data[sol]);
       });
-  }, [setSolution]);
+  }, [setAns]);
   return (
     <div>
       <nav className="border border-t-1 mt-2 border-b-1 border-gray-200">
         <h2 className="text-3xl font-bold text-center">ReactTail Wordle!</h2>
-        <h2>Solution: {solution?.word}</h2>
+        <h2>ans: {ans?.word}</h2>
       </nav>
     </div>
   );
